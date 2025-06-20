88af4ced604a3bcfa65f36651cffe1a823de0414
MUP99 <muath.aw@outlook.sa>
2025-06-20 14:58:03 +0300

    Delete setup_tracker.bat

--- setup_tracker.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-pip install pyinstaller pyqt5 opencv-python numpy pyautogui pynput pyscreeze
-pyinstaller --onefile --windowed --name BO6_AimAssist bo6_tracker_final.py
-pause
